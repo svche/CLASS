0db5c8efc773948a69eda9dcc58f1380eec629b5
Станислав <@->
2017-09-23 01:39:04 +0300

    1

--- CLASS-Lesson14/Program.cs
+++ CLASS-Lesson14/Program.cs
@@ -8,8 +8,102 @@ namespace CLASS_Lesson14
 {
     class Program
     {
+        class Person
+        {
+            public static int count = 0;
+            public string name;
+            public int age;
+            public string gender;
+
+            public void showInfo()
+            {
+                Console.WriteLine("{0}  {1}  {2} ", name, age, gender);
+            }
+
+            public Person()
+            {
+                Console.WriteLine("Person created");
+                count++;
+            }
+
+            public Person(string name, int age, string gender)
+            {
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+            }
+
+            public static void showCount()
+            {
+                Console.WriteLine(count);
+            }
+        }
+
+        class Car
+        {
+            
+            public string mark { get; set; }
+            public string colour { get; set; }
+            public Engine engine { get; set; }
+
+            private int _wheels;
+            public int wheels
+            {
+                get { return _wheels; }
+                set { _wheels = value * 2; }
+            }
+
+            public Car()
+            {
+                _wheels = 4;
+            }
+
+            public double getMaxSpeed()
+            {
+                double result = (engine.power * wheels) / 8;
+                return result;
+            }
+        }
+
+        class Engine
+        {
+            public int power { get; set;}
+            public string manufacture { get; set; }
+
+        }
+
         static void Main(string[] args)
         {
+            Engine engine = new Engine();
+            engine.manufacture = "Motors";
+            engine.power = 500;
+
+            Car huyndai = new Car();
+            huyndai.mark = "Getz";
+            huyndai.colour = "Green";
+            //huyndai.wheels = 4;
+            huyndai.engine = engine;
+
+            Console.WriteLine(huyndai.getMaxSpeed());
+            
+
+
+            /*
+            Person p1 = new Person();
+            //p1.name = "Ivan";
+            //p1.age = 32;
+            //p1.gender = "Man";
+            //p1.showInfo(); 
+            Person.showCount();
+
+            Person p2 = new Person();
+            Person.showCount();
+            Person p3 = new Person();
+            Person.showCount();
+            */
+
+
+
         }
     }
 }
---
+++ Workshop15/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop15
+{
+    class Program
+    {
+
+        class Book
+        {
+            protected string _author;
+            protected string _name;
+            protected int _pages;
+
+            public void printAll()
+            {
+                Console.Write("{0},  {1},  {2}. ", _author, _name, _pages);
+            }
+
+            public string author
+            {
+                get { return _author; }
+                set { _author = value; }
+            }
+            public string name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public int pages
+            {
+                get { return _pages; }
+                set
+                {
+                    if (value > 0) { _pages = value + 2; };
+                }
+            }
+
+            public Book()
+            {
+                _author = "unknown";
+                _name = "unknown";
+                _pages = 0;
+            }
+            public Book(string author)
+            {
+                _author = author;
+            }
+            public Book(string author, string name)
+            {
+                _author = author;
+                _name = name;
+            }
+            public Book(string author, string name, int pages)
+            {
+                _author = author;
+                _name = name;
+                _pages = pages;
+            }
+        }
+
+        class Technical_Book : Book
+        {
+            private string _fieldOfScience;
+
+            public void printAll()
+            {
+                Console.Write("{0},  {1},  {2},  {3}. ", _author, _name, _pages, _fieldOfScience);
+            }
+
+            public string fieldOfScience
+            {
+                get { return _fieldOfScience; }
+                set { _fieldOfScience = value; }
+            }
+
+            public Technical_Book() : base()
+            {
+                _fieldOfScience = "unknown";
+            }
+
+            public Technical_Book(string fieldOfScience) : base()
+            {
+                _fieldOfScience = fieldOfScience; 
+            }
+
+            public Technical_Book(string fieldOfScience, string author, string name) : base(author, name)
+            {
+                _fieldOfScience = fieldOfScience;
+            }
+            public Technical_Book(string author, string name, int pages, string fieldOfScience) : base (author, name, pages)
+            {
+                _fieldOfScience = fieldOfScience;
+            }
+
+        }
+
+        class Fuction_Book : Book
+        {
+            private string _fieldOfLiterature;
+            public void printAll()
+            {
+                Console.Write("{0},  {1},  {2},  {3}. ", _author, _name, _pages, _fieldOfLiterature);
+            }
+            public string fieldOfLiterature
+            {
+                get { return _fieldOfLiterature; }
+                set { _fieldOfLiterature = value; }
+            }
+
+            public Fuction_Book() : base ()
+            {
+                _fieldOfLiterature = "unknown";
+            }
+            public Fuction_Book(string fieldOfLiterature) : base()
+            {
+                _fieldOfLiterature = fieldOfLiterature;
+            }
+            public Fuction_Book(string fieldOfLiterature, string author, string name) : base(author, name)
+            {
+                _fieldOfLiterature = fieldOfLiterature;
+            }
+            public Fuction_Book(string author, string name, int pages, string fieldOfLiterature) : base (author, name, pages)
+            {
+                _fieldOfLiterature = fieldOfLiterature;
+            }
+        }
+
+        class Library 
+        {
+            public static List<Book> bookList = new List<Book>();
+
+            public void addBook(Book b)
+            {
+                bookList.Add(b);
+            }
+
+            public void printBookList()
+            {
+                foreach (Book b in bookList) Console.Write(b.author + ", " + b.name + ", " + b.pages + "\n");
+            }
+
+            public void printFuctioBook()
+            {
+                foreach(Book b in bookList)
+                {
+                    if (b.GetType().Name.Equals("Fuction_Book"))
+                    {
+                        Console.Write(b.author + ", " + b.name + ", " + b.pages + "\n");
+                    }
+                }
+            }
+
+            public void printTechnicalBook()
+            {
+                foreach (Book b in bookList)
+                {
+                    if (b.GetType().Name.Equals("Technical_Book"))
+                    {
+                        Console.Write(b.author + ", " + b.name + ", " + b.pages + "\n");
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Fuction_Book fb = new Fuction_Book("Rend", "Atlas Shrugged", 800, " Philosofy Novel");
+            fb.printAll();
+            Console.WriteLine();
+
+            Technical_Book tb = new Technical_Book("Shildt", "C#", 1000, "IT");
+            tb.printAll();
+            Console.WriteLine("\n");
+
+            Library l1 = new Library();
+            l1.addBook(fb);
+            l1.addBook(tb);
+
+            Console.WriteLine("All book:");
+            l1.printBookList();
+
+            Console.WriteLine("\nFuction Book:");
+            l1.printFuctioBook();
+
+            Console.WriteLine("\nTechnical Book:");
+            l1.printTechnicalBook();
+
+           
+
+            //Console.WriteLine(tb1.GetType().Name);
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            //List<int> myList = new List<int>();
+
+            //myList.Add(5);
+            //myList.Add(10);
+            //myList.Add(15);
+
+            //Console.WriteLine(myList.Count + "\n");
+
+            //foreach(int i in myList) Console.WriteLine(i);
+            //Console.WriteLine();
+
+            //myList.Remove(10);
+            //foreach (int i in myList) Console.WriteLine(i);
+            //Console.WriteLine();
+
+            //myList.RemoveAt(1);
+            //foreach (int i in myList) Console.WriteLine(i);
+
+            ////////////////////////////////////////////////////////
+            //LinkedList<int> myLinkedlist = new LinkedList<int>();
+            //myLinkedlist.AddFirst(1);
+            //myLinkedlist.AddLast(2);
+            //myLinkedlist.AddLast(3);
+
+            //foreach (int i in myLinkedlist) Console.WriteLine(i);
+            //Console.WriteLine();
+
+            //myLinkedlist.RemoveFirst();
+            //foreach (int i in myLinkedlist) Console.WriteLine(i);
+            ///////////////////////////////////////////////////////////////////////////////////////////
+
+            Console.ReadLine();
+        }
+    }
+}
